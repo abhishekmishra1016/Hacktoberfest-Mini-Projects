340afa80b1924504335cfcd1f50ed92848a8b10c
faizaslam11 <faizaslam11@gmail.com>
2020-10-19 18:52:58 +0530

    Adding Image grab

---
+++ Python Projects/image_grab.py
@@ -0,0 +1,29 @@
+import PIL
+from PIL import ImageGrab
+from PIL import Image
+import os
+import time
+import random
+
+
+time.sleep(1)
+while True:
+    try:
+        print('start')
+        img = ImageGrab.grab()
+
+        if img:
+            # change the path as per your machine
+            if os.path.exists(r"C:\\Users\\91817\\PycharmProjects\\opencv\\images"):
+                x = random.randint(1,10000)
+                y = random.randint(2,100001)
+                s = str(x) + str(y) + '.' + 'jpg'
+                pat = r"C:\\Users\\91817\\PycharmProjects\\opencv\\images"
+                r = os.path.join(pat,s)
+                print(r)
+
+                img.save(r)
+            else:
+                print('FILE NOT FOUND!')
+    except:
+        pass
